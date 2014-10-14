32ff36dc43ff8a3fba748c4ab91f20ab795e51bb
Ana Carolina <acarolinafg@gmail.com>
2014-10-14 15:02:32 -0300

    Criaçao do projeto.

---
+++ projeto/src/projeto/Main.java
@@ -0,0 +1,25 @@
+/*
+ * Universidade Federal de Juiz de Fora
+ * Instituto de Ciências Exatas
+ * Departamento de Ciência da Computação
+ * DCC012 - Estrutura de Dados
+ * Prof.: Jairo Francisco de Souza
+ * Aluno(a): Ana Carolina Fidelis Gonçalves - 200935002
+ */
+package projeto;
+
+/**
+ * Chamada dos métodos principais para execução da aplicação.
+ *
+ * @author Ana Carolina FIdelis Gonçalves <acarolinafg@gmail.com>
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
