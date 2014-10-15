5d23dc1036c2138a12d2d3ba69b8e33cc6241c18
Ana Carolina Fidelis Gonçalves <acarolinafg@gmail.com>
2014-10-15 15:23:14 -0300

    Classe que trata as geração das chaves de hash e construção da tabela hash.

---
+++ projeto/src/projeto/hash/Hash.java
@@ -0,0 +1,36 @@
+/*
+ * Universidade Federal de Juiz de Fora
+ * Instituto de Ciências Exatas
+ * Departamento de Ciência da Computação
+ * DCC012 - Estrutura de Dados
+ * Prof.: Jairo Francisco de Souza
+ * Aluno(a): Ana Carolina Fidelis Gonçalves - 200935002
+ */
+package projeto.hash;
+
+import java.util.HashMap;
+
+/**
+ * Classe responsável pelas funções de hash e contrução da tabela de hash.
+ *
+ * @author Ana Carolina FIdelis Gonçalves <acarolinafg@gmail.com>
+ */
+public class Hash {
+
+    private HashMap<String, String> tburls;
+
+    public static String hash1(String chave) {
+        return chave;
+    }
+
+    public static String hash2(String chave) {
+        return chave;
+    }
+
+    public HashMap<String, String> getTburls() {
+        return tburls;
+    }
+    
+    
+
+}
