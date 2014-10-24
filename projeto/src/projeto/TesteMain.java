2c066ec2b672e16aed86915c3d6ff87003445b07
Ana Carolina Fidelis Gonçalves <acarolinafg@gmail.com>
2014-10-24 07:18:42 -0200

    Arquivo de testes...

--- projeto/src/projeto/TesteMain.java
+++ projeto/src/projeto/TesteMain.java
@@ -2,9 +2,12 @@ package projeto;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.Set;
 import projeto.hash.Hash;
 import projeto.io.FileManager;
+import projeto.trie.abb.No;
+import projeto.trie.abb.TrieBB;
 import projeto.util.Util;
 
 /**
@@ -20,33 +23,40 @@ public class TesteMain {
     public static void main(String[] args) {
         //Manipulação do arquivo
 
-        FileManager file;
-        try {
-            file = new FileManager(args[0]);
-            //Ler arquivo
-            System.out.println("Lendo...");
-            ArrayList<String> arq = file.lerArquivo();
-            System.out.println("Tamanho do arquivo: " + arq.size());
+        /*FileManager file;
+         try {
+         file = new FileManager(args[0]);
+         //Ler arquivo
+         System.out.println("Lendo...");
+         ArrayList<String> arq = file.lerArquivo();
+         System.out.println("Tamanho do arquivo: " + arq.size());
 
-            //Tratar as urls
-            System.out.println("Tratando...");
-            Set<String> urls = Util.tratarUrl(arq);
-            System.out.println("Urls válidas: " + urls.size());
+         //Tratar as urls
+         System.out.println("Tratando...");
+         Set<String> urls = Util.tratarUrl(arq);
 
-            //Criar a tabela de hash
-            System.out.println("Criando Hash");
-            Hash.codificarUrls(urls);
-            System.out.println("Colisões:" + Hash.getColisoes());
+         //Criar a tabela de hash
+         System.out.println("Criando Hash");
+         Hash.codificarUrls(urls);
+         System.out.println("Colisões:" + Hash.getColisoes());
 
-            //Arquivo com a tabela de hash
-            System.out.println("Criando arquivo...");
-            file.criarArquivoTbHash();
+         //Arquivo com a tabela de hash
+         System.out.println("Criando arquivo...");
+         file.criarArquivoTbHash();
 
-        } catch (IOException e) {
-            System.err.println("Erro na leitura do arquivo.");
-            System.exit(0);
-        }
-        //String txt = "es.uefa.com/uefaeuropaleague/season=2012/matches/round=2000269/match=2007129/index.html";
-        //System.out.println(new ColognePhonetic().encode(txt));
+         System.out.println("Urls válidas: " + urls.size());
+            
+         } catch (IOException e) {
+         System.err.println("Erro na leitura do arquivo.");
+         System.exit(0);
+         }*/
+        
+        TrieBB arv = new TrieBB();
+        
+        System.out.println("Inseriu.");
+        arv.purge();
+        
+        System.out.println("Limpou.");
+        
     }
 }
