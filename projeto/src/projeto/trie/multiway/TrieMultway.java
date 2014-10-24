cff9e69bccbc97a56e510278f0bfe0dd6970c933
Ana Carolina Fidelis Gonçalves <acarolinafg@gmail.com>
2014-10-24 07:17:56 -0200

    Criação da classe da TrieMultway.

---
+++ projeto/src/projeto/trie/multiway/TrieMultway.java
@@ -0,0 +1,34 @@
+/*
+ * Universidade Federal de Juiz de Fora
+ * Instituto de Ciências Exatas
+ * Departamento de Ciência da Computação
+ * DCC012 - Estrutura de Dados
+ * Prof.: Jairo Francisco de Souza
+ * Aluno(a): Ana Carolina Fidelis Gonçalves - 200935002
+ */
+package projeto.trie.multiway;
+
+import projeto.trie.Trie;
+
+/**
+ * Classe de implementação da TrieMultway
+ * @author 
+ */
+public class TrieMultway implements Trie{
+
+    @Override
+    public void inserir(String dado) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String buscar(String chave) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void purge() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
